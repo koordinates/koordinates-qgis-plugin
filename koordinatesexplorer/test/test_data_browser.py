89a49c6d810d8fbaff76bc95e2b9dd728cd0675f
Nyall Dawson <nyall.dawson@gmail.com>
2022-10-04 13:58:29 +1000

    Flake

--- koordinatesexplorer/test/test_data_browser.py
+++ koordinatesexplorer/test/test_data_browser.py
@@ -44,28 +44,33 @@ class TestDataBrowser(unittest.TestCase):
         query = DataBrowserQuery()
         query.search = 'my filter'
         self.assertEqual(query.build_query(),
-                         {'sort': 'popularity', 'q': 'my filter', 'kind': ['layer', 'table', 'set', 'document']})
+                         {'sort': 'popularity', 'q': 'my filter',
+                          'kind': ['layer', 'table', 'set', 'document']})
 
     def test_starred(self):
         query = DataBrowserQuery()
         query.starred = True
         self.assertEqual(query.build_query(),
-                         {'sort': 'popularity', 'is_starred': True, 'kind': ['layer', 'table', 'set', 'document']})
+                         {'sort': 'popularity', 'is_starred': True,
+                          'kind': ['layer', 'table', 'set', 'document']})
 
     def test_access(self):
         query = DataBrowserQuery()
         query.access_type = AccessType.Private
         self.assertEqual(query.build_query(),
-                         {'sort': 'popularity', 'public': False, 'kind': ['layer', 'table', 'set', 'document']})
+                         {'sort': 'popularity', 'public': False,
+                          'kind': ['layer', 'table', 'set', 'document']})
         query.access_type = AccessType.Public
         self.assertEqual(query.build_query(),
-                         {'sort': 'popularity', 'public': True, 'kind': ['layer', 'table', 'set', 'document']})
+                         {'sort': 'popularity', 'public': True,
+                          'kind': ['layer', 'table', 'set', 'document']})
 
     def test_category(self):
         query = DataBrowserQuery()
         query.category = 'my filter'
         self.assertEqual(query.build_query(),
-                         {'sort': 'popularity', 'category': 'my filter', 'kind': ['layer', 'table', 'set', 'document']})
+                         {'sort': 'popularity', 'category': 'my filter',
+                          'kind': ['layer', 'table', 'set', 'document']})
 
     def test_data_type_to_string(self):
         self.assertEqual(DataBrowserQuery.data_type_to_string(DataType.Vectors),
@@ -90,7 +95,8 @@ class TestDataBrowser(unittest.TestCase):
         query.data_types = {DataType.Rasters}
         self.assertEqual(query.build_query(), {'sort': 'popularity', 'kind': 'raster'})
         query.data_types = {DataType.Grids}
-        self.assertEqual(query.build_query(), {'sort': 'popularity', 'kind': ['attribute-grid', 'grid']})
+        self.assertEqual(query.build_query(),
+                         {'sort': 'popularity', 'kind': ['attribute-grid', 'grid']})
         query.data_types = {DataType.PointClouds}
         self.assertEqual(query.build_query(), {'sort': 'popularity', 'kind': 'pointcloud'})
         query.data_types = {DataType.Tables}
@@ -103,19 +109,24 @@ class TestDataBrowser(unittest.TestCase):
         self.assertEqual(query.build_query(), {'sort': 'popularity', 'kind': 'document'})
         # combined
         query.data_types = {DataType.Repositories, DataType.PointClouds}
-        self.assertEqual(query.build_query(), {'sort': 'popularity', 'kind': ['pointcloud', 'repo']})
+        self.assertEqual(query.build_query(),
+                         {'sort': 'popularity', 'kind': ['pointcloud', 'repo']})
 
     def test_vector_filters(self):
         query = DataBrowserQuery()
         query.data_types = {DataType.Vectors}
         query.vector_filters = {VectorFilter.Point}
-        self.assertEqual(query.build_query(), {'sort': 'popularity', 'data.geometry_type': 'point'})
+        self.assertEqual(query.build_query(),
+                         {'sort': 'popularity', 'data.geometry_type': 'point'})
         query.vector_filters = {VectorFilter.Line}
-        self.assertEqual(query.build_query(), {'sort': 'popularity', 'data.geometry_type': 'linestring'})
+        self.assertEqual(query.build_query(),
+                         {'sort': 'popularity', 'data.geometry_type': 'linestring'})
         query.vector_filters = {VectorFilter.Polygon}
-        self.assertEqual(query.build_query(), {'sort': 'popularity', 'data.geometry_type': 'polygon'})
+        self.assertEqual(query.build_query(),
+                         {'sort': 'popularity', 'data.geometry_type': 'polygon'})
         query.vector_filters = {VectorFilter.Polygon, VectorFilter.Line}
-        self.assertEqual(query.build_query(), {'sort': 'popularity', 'data.geometry_type': ['linestring', 'polygon']})
+        self.assertEqual(query.build_query(),
+                         {'sort': 'popularity', 'data.geometry_type': ['linestring', 'polygon']})
 
         query.vector_filters = {VectorFilter.HasZ}
         self.assertEqual(query.build_query(),
@@ -130,7 +141,8 @@ class TestDataBrowser(unittest.TestCase):
         # combined
         query.vector_filters = {VectorFilter.HasZ, VectorFilter.HasPrimaryKey, VectorFilter.Point}
         self.assertEqual(query.build_query(),
-                         {'sort': 'popularity', 'data.geometry_type': 'point', 'has_pk': True, 'has_z': True})
+                         {'sort': 'popularity', 'data.geometry_type': 'point', 'has_pk': True,
+                          'has_z': True})
 
     def test_raster_filters(self):
         query = DataBrowserQuery()
@@ -146,20 +158,24 @@ class TestDataBrowser(unittest.TestCase):
                          {'sort': 'popularity', 'kind': 'raster'})
         query.raster_band_filters = {RasterBandFilter.RGB}
         self.assertEqual(query.build_query(),
-                         {'sort': 'popularity', 'raster_band': ['blue', 'green', 'red'], 'kind': 'raster'})
+                         {'sort': 'popularity', 'raster_band': ['blue', 'green', 'red'],
+                          'kind': 'raster'})
         query.raster_band_filters = {RasterBandFilter.BlackAndWhite}
         self.assertEqual(query.build_query(),
                          {'sort': 'popularity', 'raster_band': 'gray', 'kind': 'raster'})
         query.raster_filter_options = {RasterFilterOptions.WithAlphaChannel}
         self.assertEqual(query.build_query(),
-                         {'sort': 'popularity', 'raster_band': ['alpha', 'gray'], 'kind': 'raster'})
+                         {'sort': 'popularity', 'raster_band': ['alpha', 'gray'],
+                          'kind': 'raster'})
         query.raster_band_filters = {RasterBandFilter.RGB}
         self.assertEqual(query.build_query(),
-                         {'sort': 'popularity', 'raster_band': ['alpha', 'blue', 'green', 'red'], 'kind': 'raster'})
+                         {'sort': 'popularity', 'raster_band': ['alpha', 'blue', 'green', 'red'],
+                          'kind': 'raster'})
 
         query.raster_filters = {RasterFilter.NotAerialSatellitePhotos}
         self.assertEqual(query.build_query(),
-                         {'sort': 'popularity', 'is_imagery': False, 'raster_band': 'alpha', 'kind': 'raster'})
+                         {'sort': 'popularity', 'is_imagery': False, 'raster_band': 'alpha',
+                          'kind': 'raster'})
 
         query.data_types = {DataType.Rasters}
 
@@ -183,45 +199,54 @@ class TestDataBrowser(unittest.TestCase):
                                                'raster_resolution.lt': 30000})
         query.maximum_resolution = 20000
         self.assertEqual(query.build_query(),
-                         {'sort': 'popularity', 'is_imagery': False, 'kind': 'raster', 'raster_band': 'alpha'})
+                         {'sort': 'popularity', 'is_imagery': False, 'kind': 'raster',
+                          'raster_band': 'alpha'})
 
     def test_grid_filters(self):
         query = DataBrowserQuery()
         query.data_types = {DataType.Grids}
-        self.assertEqual(query.build_query(), {'sort': 'popularity', 'kind': ['attribute-grid', 'grid']})
+        self.assertEqual(query.build_query(),
+                         {'sort': 'popularity', 'kind': ['attribute-grid', 'grid']})
         query.grid_filter_options = {GridFilterOptions.MultiAttributeGridsOnly}
         self.assertEqual(query.build_query(), {'sort': 'popularity', 'kind': 'attribute-grid'})
         query.data_types = {DataType.Grids, DataType.Rasters}
-        self.assertEqual(query.build_query(), {'sort': 'popularity', 'kind': ['attribute-grid', 'raster']})
+        self.assertEqual(query.build_query(),
+                         {'sort': 'popularity', 'kind': ['attribute-grid', 'raster']})
 
     def test_table_filter(self):
         query = DataBrowserQuery()
         query.data_types = {DataType.Tables}
         self.assertEqual(query.build_query(), {'sort': 'popularity', 'kind': 'table'})
         query.vector_filters.add(VectorFilter.HasPrimaryKey)
-        self.assertEqual(query.build_query(), {'sort': 'popularity', 'kind': 'table', 'has_pk': True})
+        self.assertEqual(query.build_query(),
+                         {'sort': 'popularity', 'kind': 'table', 'has_pk': True})
 
     def test_date_filters(self):
         query = DataBrowserQuery()
         query.created_minimum = QDateTime(QDate(2022, 1, 3), QTime(0, 0, 0))
-        self.assertEqual(query.build_query(), {'sort': 'popularity', 'created_at.after': '2022-01-03T00:00:00',
-                                               'kind': ['layer', 'table', 'set', 'document']})
+        self.assertEqual(query.build_query(),
+                         {'sort': 'popularity', 'created_at.after': '2022-01-03T00:00:00',
+                          'kind': ['layer', 'table', 'set', 'document']})
         query.created_minimum = None
         query.created_maximum = QDateTime(QDate(2022, 1, 3), QTime(0, 0, 0))
-        self.assertEqual(query.build_query(), {'sort': 'popularity', 'created_at.before': '2022-01-03T00:00:00',
-                                               'kind': ['layer', 'table', 'set', 'document']})
+        self.assertEqual(query.build_query(),
+                         {'sort': 'popularity', 'created_at.before': '2022-01-03T00:00:00',
+                          'kind': ['layer', 'table', 'set', 'document']})
         query.created_maximum = None
         query.updated_minimum = QDateTime(QDate(2022, 1, 3), QTime(0, 0, 0))
-        self.assertEqual(query.build_query(), {'sort': 'popularity', 'updated_at.after': '2022-01-03T00:00:00',
-                                               'kind': ['layer', 'table', 'set', 'document']})
+        self.assertEqual(query.build_query(),
+                         {'sort': 'popularity', 'updated_at.after': '2022-01-03T00:00:00',
+                          'kind': ['layer', 'table', 'set', 'document']})
         query.updated_minimum = None
         query.updated_maximum = QDateTime(QDate(2022, 1, 3), QTime(0, 0, 0))
-        self.assertEqual(query.build_query(), {'sort': 'popularity', 'updated_at.before': '2022-01-03T00:00:00',
-                                               'kind': ['layer', 'table', 'set', 'document']})
+        self.assertEqual(query.build_query(),
+                         {'sort': 'popularity', 'updated_at.before': '2022-01-03T00:00:00',
+                          'kind': ['layer', 'table', 'set', 'document']})
         query.updated_minimum = QDateTime(QDate(2020, 1, 3), QTime(0, 0, 0))
-        self.assertEqual(query.build_query(), {'sort': 'popularity', 'updated_at.after': '2020-01-03T00:00:00',
-                                               'updated_at.before': '2022-01-03T00:00:00',
-                                               'kind': ['layer', 'table', 'set', 'document']})
+        self.assertEqual(query.build_query(),
+                         {'sort': 'popularity', 'updated_at.after': '2020-01-03T00:00:00',
+                          'updated_at.before': '2022-01-03T00:00:00',
+                          'kind': ['layer', 'table', 'set', 'document']})
 
     def test_license_filters(self):
         query = DataBrowserQuery()
@@ -234,41 +259,49 @@ class TestDataBrowser(unittest.TestCase):
                                                'kind': ['layer', 'table', 'set', 'document']})
         query.cc_license_versions = {CreativeCommonLicenseVersions.Version3,
                                      CreativeCommonLicenseVersions.Version4}
-        self.assertEqual(query.build_query(), {'sort': 'popularity', 'license.version': ['3.0', '4.0'],
-                                               'kind': ['layer', 'table', 'set', 'document']})
+        self.assertEqual(query.build_query(),
+                         {'sort': 'popularity', 'license.version': ['3.0', '4.0'],
+                          'kind': ['layer', 'table', 'set', 'document']})
 
         query.cc_license_versions = {CreativeCommonLicenseVersions.Version3}
         query.cc_license_allow_derivates = True
         self.assertEqual(query.build_query(),
-                         {'sort': 'popularity', 'license.type': '-cc-by-nd', 'license.version': '3.0',
+                         {'sort': 'popularity', 'license.type': '-cc-by-nd',
+                          'license.version': '3.0',
                           'kind': ['layer', 'table', 'set', 'document']})
         query.cc_license_allow_derivates = False
         self.assertEqual(query.build_query(),
-                         {'sort': 'popularity', 'license.type': 'cc-by-nd', 'license.version': '3.0',
+                         {'sort': 'popularity', 'license.type': 'cc-by-nd',
+                          'license.version': '3.0',
                           'kind': ['layer', 'table', 'set', 'document']})
         query.cc_license_allow_derivates = None
 
         query.cc_license_allow_commercial = True
         self.assertEqual(query.build_query(),
-                         {'sort': 'popularity', 'license.type': '-cc-by-nc', 'license.version': '3.0',
+                         {'sort': 'popularity', 'license.type': '-cc-by-nc',
+                          'license.version': '3.0',
                           'kind': ['layer', 'table', 'set', 'document']})
         query.cc_license_allow_commercial = False
         self.assertEqual(query.build_query(),
-                         {'sort': 'popularity', 'license.type': 'cc-by-nc', 'license.version': '3.0',
+                         {'sort': 'popularity', 'license.type': 'cc-by-nc',
+                          'license.version': '3.0',
                           'kind': ['layer', 'table', 'set', 'document']})
         query.cc_license_allow_commercial = None
 
         query.cc_license_changes_must_be_shared = True
         self.assertEqual(query.build_query(),
-                         {'sort': 'popularity', 'license.type': 'cc-by-sa', 'license.version': '3.0',
+                         {'sort': 'popularity', 'license.type': 'cc-by-sa',
+                          'license.version': '3.0',
                           'kind': ['layer', 'table', 'set', 'document']})
         query.cc_license_changes_must_be_shared = False
         self.assertEqual(query.build_query(),
-                         {'sort': 'popularity', 'license.type': '-cc-by-sa', 'license.version': '3.0',
+                         {'sort': 'popularity', 'license.type': '-cc-by-sa',
+                          'license.version': '3.0',
                           'kind': ['layer', 'table', 'set', 'document']})
         query.cc_license_allow_commercial = True
         self.assertEqual(query.build_query(),
-                         {'sort': 'popularity', 'license.type': ['-cc-by-nc', '-cc-by-sa'], 'license.version': '3.0',
+                         {'sort': 'popularity', 'license.type': ['-cc-by-nc', '-cc-by-sa'],
+                          'license.version': '3.0',
                           'kind': ['layer', 'table', 'set', 'document']})
 
     def test_sort(self):
