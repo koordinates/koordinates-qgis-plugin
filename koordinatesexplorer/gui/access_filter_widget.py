20b89f3df5680d9512ccbf51f3fb975bb5e1b40d
Nyall Dawson <nyall.dawson@gmail.com>
2022-10-07 15:29:27 +1000

    Allow both public and private buttons to be unchecked, default to this
    Fixes #30

--- koordinatesexplorer/gui/access_filter_widget.py
+++ koordinatesexplorer/gui/access_filter_widget.py
@@ -32,26 +32,37 @@ class AccessFilterWidget(FilterWidgetComboBase):
         self.access_group = QButtonGroup()
         self.access_group.addButton(self.public_radio)
         self.access_group.addButton(self.private_radio)
+        self.access_group.setExclusive(False)
+        self.access_group.buttonClicked.connect(self._access_group_member_clicked)
 
         self.drop_down_widget.setLayout(vl)
 
         self.set_contents_widget(self.drop_down_widget)
 
-        self.access_group.buttonClicked.connect(self._update_value)
-
         self.clear()
+
+    def _access_group_member_clicked(self, clicked_button):
+        self._block_changes += 1
+        for radio in (self.public_radio,
+                      self.private_radio):
+            if radio.isChecked() and radio != clicked_button:
+                radio.setChecked(False)
+
+        self._block_changes -= 1
+        self._update_value()
 
     def _update_visible_frames(self):
         self.drop_down_widget.adjustSize()
         self._floating_widget.reflow()
 
     def clear(self):
-        self.public_radio.setChecked(True)
+        self.public_radio.setChecked(False)
+        self.private_radio.setChecked(False)
         self._update_visible_frames()
         self._update_value()
 
     def should_show_clear(self):
-        if self.public_radio.isChecked():
+        if not self.public_radio.isChecked() and not self.private_radio.isChecked():
             return False
 
         return super().should_show_clear()
@@ -59,7 +70,9 @@ class AccessFilterWidget(FilterWidgetComboBase):
     def _update_value(self):
         text = 'Access'
 
-        if self.private_radio.isChecked():
+        if self.public_radio.isChecked():
+            text = 'Only public data'
+        elif self.private_radio.isChecked():
             text = 'Shared with me'
 
         self.set_current_text(text)
@@ -69,16 +82,16 @@ class AccessFilterWidget(FilterWidgetComboBase):
     def apply_constraints_to_query(self, query: DataBrowserQuery):
         if self.public_radio.isChecked():
             query.access_type = AccessType.Public
-        else:
+        elif self.private_radio.isChecked():
             query.access_type = AccessType.Private
+        else:
+            query.access_type = None
 
     def set_from_query(self, query: DataBrowserQuery):
         self._block_changes += 1
 
-        if query.access_type == AccessType.Public:
-            self.public_radio.setChecked(True)
-        else:
-            self.private_radio.setChecked(True)
+        self.public_radio.setChecked(query.access_type == AccessType.Public)
+        self.private_radio.setChecked(query.access_type == AccessType.Private)
 
         self._update_value()
         self._update_visible_frames()
