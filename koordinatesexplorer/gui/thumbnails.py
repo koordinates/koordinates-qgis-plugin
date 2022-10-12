91948298ee9d7cd808763160652b94a5a09c2a13
Nyall Dawson <nyall.dawson@gmail.com>
2022-10-12 15:08:33 +1000

    Use correct network access manager for thumbnail requests

--- koordinatesexplorer/gui/thumbnails.py
+++ koordinatesexplorer/gui/thumbnails.py
@@ -1,25 +1,32 @@
 from collections import defaultdict
 
-from PyQt5.QtNetwork import QNetworkAccessManager, QNetworkReply, QNetworkRequest
+from functools import partial
+from qgis.PyQt.QtNetwork import QNetworkReply, QNetworkRequest
+from qgis.core import QgsNetworkAccessManager
 from qgis.PyQt.QtCore import QUrl
 from qgis.PyQt.QtGui import QImage
 
 
 class ThumbnailManager:
     def __init__(self):
-        self.nam = QNetworkAccessManager()
-        self.nam.finished.connect(self.thumbnailDownloaded)
         self.thumbnails = {}
         self.widgets = defaultdict(list)
+        self.queued_replies = set()
 
     def downloadThumbnail(self, url, widget):
         if url in self.thumbnails:
             widget.setThumbnail(self.thumbnails[url])
         else:
             self.widgets[url].append(widget)
-            self.nam.get(QNetworkRequest(QUrl(url)))
+            reply = QgsNetworkAccessManager.instance().get(QNetworkRequest(QUrl(url)))
+            self.queued_replies.add(reply)
+            if reply.isFinished():
+                self.thumbnailDownloaded(reply)
+            else:
+                reply.finished.connect(partial(self.thumbnailDownloaded, reply))
 
     def thumbnailDownloaded(self, reply):
+        self.queued_replies.remove(reply)
         if reply.error() == QNetworkReply.NoError:
             url = reply.url().toString()
             img = QImage()
