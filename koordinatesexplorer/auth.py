80b276258b3bd612aebddc76b966b9071197ae46
Nyall Dawson <nyall.dawson@gmail.com>
2022-11-07 13:13:13 +1000

    Overwrite base HTTPServer logging mechanism
    Fixes auth success page timeouts on windows

--- koordinatesexplorer/auth.py
+++ koordinatesexplorer/auth.py
@@ -66,6 +66,9 @@ class AuthState(Enum):
 
 class _Handler(BaseHTTPRequestHandler):
 
+    def log_request(self, format, *args):
+        pass
+
     def do_GET(self):
         params = parse_qs(urlsplit(self.path).query)
         code = params.get("code")
