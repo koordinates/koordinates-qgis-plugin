dadc84829ff1cbf26fbcdfe0f593df26b8ead586
Nyall Dawson <nyall.dawson@gmail.com>
2023-06-08 10:39:18 +1000

    Fix thumbnail appearance in context switcher
    Fixes #219

--- koordinates/gui/thumbnails.py
+++ koordinates/gui/thumbnails.py
@@ -16,7 +16,12 @@ from qgis.PyQt.QtCore import (
 from qgis.PyQt.QtNetwork import QNetworkReply, QNetworkRequest
 from qgis.core import QgsNetworkAccessManager
 from qgis.PyQt.QtCore import QUrl
-from qgis.PyQt.QtGui import QImage
+from qgis.PyQt.QtGui import (
+    QImage,
+    QColor,
+    QPainter,
+    QBrush
+)
 
 from ..api import (
     Publisher,
@@ -35,6 +40,80 @@ class ThumbnailProcessor(ABC):
         """
         Processes the thumbnail
         """
+
+
+class UserThumbnailProcessor(ThumbnailProcessor):
+    """
+    A thumbnail processor for user thumbnails
+    """
+
+    def __init__(self,
+                 name: str,
+                 size: QSize):
+        self.name = name
+        self.size = size
+
+    def process_thumbnail(self, thumbnail: Optional[QImage]) -> QImage:
+        from .dataset_utils import DatasetGuiUtils
+        from .user_avatar_generator import UserAvatarGenerator
+
+        if thumbnail and not thumbnail.isNull():
+            scaled = DatasetGuiUtils.crop_image_to_circle(
+                thumbnail, self.size.height()
+            )
+            return scaled
+
+        return UserAvatarGenerator.get_avatar(
+            self.name,
+            self.size.height()
+        )
+
+
+class PublisherTypeThumbnailProcessor(ThumbnailProcessor):
+    """
+    A thumbnail processor for publisher type thumbnails
+    """
+
+    def __init__(self,
+                 size: QSize,
+                 background_color: Optional[QColor] = None):
+        self.size = size
+        self.background_color = background_color
+
+    def process_thumbnail(self, thumbnail: Optional[QImage]) -> QImage:
+        max_thumbnail_width = self.size.width()
+        max_thumbnail_height = \
+            int(min(self.size.height(),
+                    thumbnail.height()))
+        scaled = thumbnail.scaled(
+            QSize(max_thumbnail_width, max_thumbnail_height),
+            Qt.KeepAspectRatio,
+            Qt.SmoothTransformation)
+
+        if self.background_color:
+            with_background = QImage(self.size,
+                                     QImage.Format_ARGB32_Premultiplied)
+            with_background.fill(Qt.transparent)
+            painter = QPainter(with_background)
+            painter.setRenderHint(QPainter.Antialiasing, True)
+
+            painter.setBrush(QBrush(self.background_color))
+            painter.drawRoundedRect(0, 0,
+                                    self.size.width(),
+                                    self.size.height(),
+                                    3,
+                                    3)
+
+            x_center = int((self.size.width() - scaled.width()) / 2)
+            y_center = int((self.size.height() - scaled.height()) / 2)
+            painter.drawImage(x_center, y_center, scaled)
+
+
+            painter.end()
+
+            scaled = with_background
+
+        return scaled
 
 
 class PublisherThumbnailProcessor(ThumbnailProcessor):
@@ -56,26 +135,23 @@ class PublisherThumbnailProcessor(ThumbnailProcessor):
 
     def process_thumbnail(self, thumbnail: Optional[QImage]) -> QImage:
         from .dataset_utils import DatasetGuiUtils
-        from .user_avatar_generator import UserAvatarGenerator
+
+        if self.publisher.publisher_type == PublisherType.User:
+            return UserThumbnailProcessor(
+                self.publisher.name(),
+                self.size).process_thumbnail(thumbnail)
 
         if thumbnail and not thumbnail.isNull():
             if self.publisher.publisher_type == PublisherType.Publisher:
-                max_thumbnail_width = self.size.width()
-                max_thumbnail_height = \
-                    int(min(self.size.height(),
-                            thumbnail.height()))
-                scaled = thumbnail.scaled(
-                    QSize(max_thumbnail_width, max_thumbnail_height),
-                    Qt.KeepAspectRatio,
-                    Qt.SmoothTransformation)
+                return PublisherTypeThumbnailProcessor(
+                    self.size
+                ).process_thumbnail(thumbnail)
             else:
                 scaled = DatasetGuiUtils.crop_image_to_circle(
                     thumbnail, thumbnail.height()
                 )
             return scaled
 
-        elif self.publisher.publisher_type == PublisherType.User:
-            thumbnail = UserAvatarGenerator.get_avatar(self.publisher.name())
         else:
             thumbnail = GuiUtils.get_svg_as_image('globe.svg', 40, 40)
 
@@ -129,6 +205,11 @@ class ThumbnailManager:
                           url: str,
                           widget,
                           processor: Optional[ThumbnailProcessor] = None):
+        if not url and processor:
+            thumbnail = processor.process_thumbnail(None)
+            widget.setThumbnail(thumbnail)
+            return
+
         if url in self.thumbnails:
             thumbnail = self.thumbnails[url]
             if processor:
